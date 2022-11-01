263dbe81dd0f7badf847b559a5b6addcd2705a53
Jem <0x0xjem@gmail.com>
2022-11-01 13:03:15 +0400

    Generating BigQuery schema

--- src/helpers/bigquerySchema.ts
+++ src/helpers/bigquerySchema.ts
@@ -1,25 +1,11 @@
 import $RefParser = require("@apidevtools/json-schema-ref-parser");
-import { writeFileSync } from "fs";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import * as jsonSchemaBigQuery from "jsonschema-bigquery";
-import { resolve } from "path";
-import * as TJS from "typescript-json-schema";
 
-export const getBigQuerySchema = async (typesFilePath: string): Promise<string> => {
-  const jsonSchemaProgram = TJS.getProgramFromFiles([resolve(typesFilePath)], {
-    strictNullChecks: true,
-  });
-  const jsonSchema = TJS.generateSchema(jsonSchemaProgram, "TokenHolderTransaction");
-  console.log(`schema = ${JSON.stringify(jsonSchema, null, 2)}`);
-  writeFileSync("schema.jsonschema", JSON.stringify(jsonSchema, null, 2));
-
-  const derefSchema = await $RefParser.dereference("schema.jsonschema");
-
-  console.log(`new schema = ${JSON.stringify(derefSchema, null, 2)}`);
-
+export const getBigQuerySchema = async (schema: $RefParser.JSONSchema): Promise<string> => {
   // Convert to BigQuery schema: https://github.com/thedumbterminal/jsonschema-bigquery
-  const bigQuerySchema = jsonSchemaBigQuery.run(derefSchema);
+  const bigQuerySchema = jsonSchemaBigQuery.run(schema);
 
-  return JSON.stringify(bigQuerySchema);
+  return JSON.stringify(bigQuerySchema, null, 2);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@urql/core";
 import fetch from "cross-fetch";
 
+import { getBigQuerySchema } from "./helpers/bigquerySchema";
 import { generateJSONSchema } from "./helpers/subgraphSchema";
 
 const SUBGRAPH_URL = "https://api.studio.thegraph.com/query/28103/token-holders/0.0.40";
@@ -33,11 +34,11 @@ export const handler = async (
     fetch,
   });
 
-  generateJSONSchema(SUBGRAPH_URL, "TokenHolderTransaction", "./src/graphql/generated.ts");
+  const schema = await generateJSONSchema(SUBGRAPH_URL, "TokenHolderTransaction", "./src/graphql/generated.ts");
 
   // console.log(`query = ${generateQuery(SUBGRAPH_URL, "TokenHolderTransaction")}`);
 
-  // getBigQuerySchema("./src/graphql/generated.ts");
+  console.log(`bq = ${await getBigQuerySchema(schema)}`);
 
   return;
 
