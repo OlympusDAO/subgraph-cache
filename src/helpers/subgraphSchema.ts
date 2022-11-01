303b8185fd5d9566a231ad5656815a9a46ad0e58
Jem <0x0xjem@gmail.com>
2022-11-01 11:24:41 +0400

    Working GraphQL query generation

--- src/helpers/subgraphSchema.ts
+++ src/helpers/subgraphSchema.ts
@@ -3,11 +3,12 @@ import { UrlLoader } from "@graphql-tools/url-loader";
 import {
   buildClientSchema,
   getIntrospectionQuery,
+  GraphQLEnumType,
   GraphQLObjectType,
   GraphQLSchema,
   graphqlSync,
   IntrospectionQuery,
-  isCompositeType,
+  isEnumType,
   isObjectType,
 } from "graphql";
 
@@ -22,14 +23,14 @@ const generateSchema = (url: string): GraphQLSchema => {
 
   const introspection = graphqlSync({ schema: schema, source: getIntrospectionQuery() });
   if (!introspection.data) {
-    throw new Error("Oops");
+    throw new Error(`Unable to run introspection query for subgraph: ${url}`);
   }
 
   const introspectionData = introspection.data as unknown as IntrospectionQuery;
   return buildClientSchema(introspectionData);
 };
 
-export const getObjectType = (url: string, type: string): GraphQLObjectType => {
+const getTypeFromSchema = (url: string, type: string): GraphQLObjectType | GraphQLEnumType => {
   const graphQLSchema: GraphQLSchema = generateSchema(url);
   const graphQLType = graphQLSchema.getType(type);
   if (!graphQLType) {
@@ -38,39 +39,91 @@ export const getObjectType = (url: string, type: string): GraphQLObjectType => {
     );
   }
 
-  if (!isObjectType(graphQLType)) {
-    throw new Error(`Type ${type} is not an object type`);
+  if (isObjectType(graphQLType)) {
+    return graphQLType as GraphQLObjectType;
   }
 
-  console.log(`type = ${JSON.stringify(graphQLType.getFields(), null, 2)}`);
+  if (isEnumType(graphQLType)) {
+    return graphQLType as GraphQLEnumType;
+  }
 
-  return graphQLType as GraphQLObjectType;
+  throw new Error(`Type ${type} is not a handled type`);
 };
 
-export const hasField = (type: GraphQLObjectType, field: string): boolean => {
+const hasField = (type: GraphQLObjectType, field: string): boolean => {
   return Object.keys(type.getFields()).includes(field);
 };
 
-const generateQueryFields = (url: string, type: string): string => {
-  const objectType = getObjectType(url, type);
-  return Object.values(objectType.getFields()).reduce((prev, field) => {
-    console.log(`field name = ${field.name}`);
-    console.log(`type = ${field.type}`);
-    console.log(`is type = ${isCompositeType(field.type)}`);
-    return `${prev}${isObjectType(field.type) ? generateQueryFields(url, field.name) : field.name}\n`;
-  }, "");
+/**
+ * Generates a fragment of the GraphQL query for the given type.
+ *
+ * This function will recurse (if necessary) for references entities
+ *
+ * @param url subgraph URL
+ * @param type the defined type (e.g. TokenHolder)
+ * @param fieldName for referenced entities, the type is different to the field name, so it is specified here
+ * @returns
+ */
+const generateQueryFields = (url: string, type: string, fieldName?: string): string => {
+  console.log(`url = ${url}, type = ${type}, field = ${fieldName}`);
+  const SCALAR_TYPES: string[] = ["String", "BigDecimal", "BigInt", "Bytes", "ID"];
+
+  const objectType = getTypeFromSchema(url, type);
+
+  if (objectType instanceof GraphQLObjectType) {
+    /**
+     * If there is an object/entity type, we need to produce a query in the following format:
+     *
+     * fieldName {
+     *   subFieldName1
+     *   subFieldName2
+     * }
+     *
+     * When the root object type is specified, fieldName is undefined, so we also cater to that.
+     */
+    return `${fieldName ? `${fieldName} {\n` : ""}${Object.values(objectType.getFields()).reduce(
+      (subQueryString, subFieldName) => {
+        // The field type includes the nullable status, so we exclude that
+        const fieldType = subFieldName.type.toString().replace("!", "");
+        const isScalar = SCALAR_TYPES.includes(fieldType);
+        // If the item is a scalar, then the name is returned, otherwise we recurse
+        return `${subQueryString}${
+          isScalar ? subFieldName.name : generateQueryFields(url, fieldType, subFieldName.name)
+        }\n`;
+      },
+      "",
+    )}${fieldName ? `}` : ""}`;
+  }
+
+  if (objectType instanceof GraphQLEnumType && !fieldName) {
+    throw new Error(`Type ${type} is an enum type, but no field name was passed`);
+  }
+
+  // If we hit an enum type (stored as strings), we just return the field name
+  if (objectType instanceof GraphQLEnumType) {
+    return `${fieldName}\n`;
+  }
+
+  throw new Error(`Unsure how to handle type ${type}`);
 };
 
+/**
+ * Construct an SDL query for the given subgraph and entity. This will
+ * introspection the subgraph schema and produce a query for all fields.
+ *
+ * @param url subgraph url
+ * @param type the name of the entity
+ * @returns SDL query represented as a string
+ * @throws if the entity cannot be found
+ */
 export const generateQuery = (url: string, type: string): string => {
   const query = `query {
-    ${toLowerCaseFirstCharacter(type)} {
+    ${toLowerCaseFirstCharacter(type) + "s"} {
         ${generateQueryFields(url, type)}
     }
   }`;
   return query;
 };
-
-// Construct SDL query
 
 // Convert types betwen GraphQLSchema and BQ
 
